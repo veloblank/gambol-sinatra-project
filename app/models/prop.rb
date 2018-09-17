cf42e9bf574d2a569e4f6e3c7c7b62332344f1f6
veloblank <veloblank@gmail.com>
2018-09-17 15:30:13 -0400

    Fix validation.

--- app/models/prop.rb
+++ app/models/prop.rb
@@ -2,7 +2,7 @@ class Prop < ActiveRecord::Base
   has_many :betting_slip_props
   has_many :users, through: :betting_slip_props
   validates_presence_of :title, :odds, :risk
-  validates_numericality of :odds, :risk
+  validates_numericality_of :odds, :risk
 
 
 end
