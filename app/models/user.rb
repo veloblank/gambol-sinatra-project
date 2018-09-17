86abd88dd4cabe56cc24a79e15393acc941e5c76
veloblank <veloblank@gmail.com>
2018-09-17 14:22:58 -0400

    Fix associations of has_many_through to use symbol.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   has_many :betting_slips
   has_many :betting_slip_props
-  has_many :props, through: betting_slips
+  has_many :props, through: :betting_slips
 
 
 
