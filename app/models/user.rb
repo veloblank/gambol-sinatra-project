bfbbe730b9af8841cfc06f7207f82a53f76b9c0c
veloblank <veloblank@gmail.com>
2018-09-17 15:08:48 -0400

    Validates presence of User attributes and 'bcrypt' security.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,13 @@
 class User < ActiveRecord::Base
+  has_secure_password
   has_many :betting_slips
   has_many :betting_slip_props
   has_many :props, through: :betting_slips
   validates :username, uniqueness: true
+  validates :username, presence: true
   validates :email, uniqueness: true
+  validates :email, presence: true
+  validates :password_digest, presence: true
 
 
 
