def9b6fc30b0674e6234e655117a12a763b258fb
veloblank <veloblank@gmail.com>
2018-09-17 14:21:00 -0400

    Create models and associations. Migrate tables.

---
+++ app/models/prop.rb
@@ -0,0 +1,6 @@
+class Prop < ActiveRecord::Base
+  has_many :betting_slip_props
+  has_many :users, through: betting_slip_props
+
+
+end
---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ActiveRecord::Base
+  has_many :betting_slips
+  has_many :betting_slip_props
+  has_many :props, through: betting_slips
+
+
+
+
+
+
+end
