96968721a31acc4dc1c2e0eed194c87bf0da856d
veloblank <veloblank@gmail.com>
2018-09-17 15:27:58 -0400

    Validate numericality of several prop attributes integral to calculating

--- app/models/prop.rb
+++ app/models/prop.rb
@@ -1,6 +1,8 @@
 class Prop < ActiveRecord::Base
   has_many :betting_slip_props
   has_many :users, through: :betting_slip_props
+  validates_presence_of :title, :odds, :risk
+  validates_numericality of :odds, :risk
 
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ActiveRecord::Base
   has_secure_password
+  validates_uniqueness of :username, :email
+  validates_presence_of :username, :email, :password_digest
   has_many :betting_slips
   has_many :betting_slip_props
   has_many :props, through: :betting_slips
-  validates_uniqueness of :username, :email
-  validates_presence_of :username, :email, :password_digest
 
 
 
