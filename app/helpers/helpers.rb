fd0062f27923b308ffd4424f94f99c36e68ec8b7
veloblank <veloblank@gmail.com>
2018-09-18 16:54:11 -0400

    Create helper class and create current_user and logged_in? methods

---
+++ app/helpers/helpers.rb
@@ -0,0 +1,14 @@
+class Helpers
+
+  def self.current_user(session_hash)
+    user = User.find_by(id: session_hash[:user_id])
+    return user if user
+  end
+
+
+  def self.is_logged_in?(session_hash)
+    !!current_user(session_hash)
+  end
+
+
+end
