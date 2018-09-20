6775ed424d5d00d588900bbdbeb4c8895dc21977
veloblank <veloblank@gmail.com>
2018-09-20 12:45:12 -0400

    Nav header reflects if user is already logged in by showing 'Logout'

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   get '/users/dashboard' do
     user = User.find_by(id: session[:user_id])
     if user && user == current_user(session)
-      "Hola #{user.username}"
+       erb :"users/dashboard"
     else
       redirect '/'
     end
