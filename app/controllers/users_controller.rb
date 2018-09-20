a66387a3afa45da5b86812bb01364fb565fddd91
veloblank <veloblank@gmail.com>
2018-09-20 11:39:21 -0400

    Fix 'nil' && current_user conditional logic.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,15 @@
 class UsersController < ApplicationController
 
-  get '/users' do
-    "Hola"
+  get '/users/dashboard' do
+    user = User.find_by(id: session[:user_id])
+    if user && user == current_user(session)
+      "Hola #{user.username}"
+    else
+      redirect '/'
+    end
   end
+
+
 
 
 
