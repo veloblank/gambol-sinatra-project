753b87b0469615a90203d20d49a76fe631f111d1
veloblank <veloblank@gmail.com>
2018-09-20 11:39:49 -0400

    Move helpers to application controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -35,6 +35,14 @@ class ApplicationController < Sinatra::Base
 
 
   helpers do
+    def current_user(session_hash)
+      user = User.find_by(id: session_hash[:user_id])
+      return user if user
+    end
+
+    def is_logged_in?(session_hash)
+      !!current_user(session_hash)
+    end
 
 
   end
