f6ef4ad218b2d6937c446e700f7bc044d3031374
veloblank <veloblank@gmail.com>
2018-09-22 14:47:01 -0400

    Create routes and 'new' to create new betting_lists

--- app/controllers/betting_slips_controller.rb
+++ app/controllers/betting_slips_controller.rb
@@ -1,5 +1,13 @@
 class BettingSlipsController < ApplicationController
 
+  get '/betting-slips/new' do
+    erb :"betting-slips/new"
+  end
 
+  post '/betting_slips' do
+    betting-slip = BettingSlip.new(params)
+    betting-slip.valid?
+    
+  end
 
 end
