431196e585a302151196c9a0d8b5ffd817908434
veloblank <veloblank@gmail.com>
2018-09-23 19:35:45 -0400

    Fix issue with cookie size being too large to handle props by using 'id'

--- app/controllers/props_controller.rb
+++ app/controllers/props_controller.rb
@@ -21,14 +21,23 @@ class PropsController < ApplicationController
   end
 
   get '/props/:id/add' do
-    prop = Prop.find_by(id: params["id"])
-    session["selected_props"] << prop
-    redirect '/props'
+    if is_logged_in?(session)
+      prop = Prop.find_by(id: params["id"])
+      session["selected_props"] << prop.id
+      redirect '/props'
+    else
+      redirect '/'
+    end
   end
 
   get '/prop_errors' do
     erb :prop_errors
 
+  end
+
+  get '/props/clear_pending_picks' do
+    session["selected_props"] = []
+    redirect '/props'
   end
 
   get '/props/:id' do
--- app/models/prop.rb
+++ app/models/prop.rb
@@ -1,6 +1,7 @@
 class Prop < ActiveRecord::Base
   has_many :betting_slip_props
   has_many :users, through: :betting_slip_props
+  
   validates_presence_of :title, :description, :odds
   validates_numericality_of :odds, only_float: true
 
