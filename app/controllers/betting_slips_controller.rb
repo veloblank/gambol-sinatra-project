ad74ace3b357d73a89db8db62d3ac505bf403608
veloblank <veloblank@gmail.com>
2018-09-22 20:32:55 -0400

    Created betting list index to view all lists and accompanying route

--- app/controllers/betting_slips_controller.rb
+++ app/controllers/betting_slips_controller.rb
@@ -20,6 +20,16 @@ class BettingSlipsController < ApplicationController
     end
   end
 
+  get '/betting-slips' do
+    if is_logged_in?(session)
+      @user = current_user(session)
+      @slips = @user.betting_slips.all
+      erb :"betting-slips/index"
+    else
+      redirect '/'
+    end
+  end
+
   get '/users/:id/betting-slips/:slip_num' do
     if is_logged_in?(session)
       @slip = BettingSlip.find_by(id: params[:slip_num])
@@ -29,6 +39,12 @@ class BettingSlipsController < ApplicationController
         redirect "prop_errors"
       end
     end
+  end
+
+  delete '/betting-slips/:id/delete' do
+    betting_slip = BettingSlip.find_by(id: params[:id])
+    betting_slip.destroy
+    redirect '/betting-slips'
   end
 
 end
