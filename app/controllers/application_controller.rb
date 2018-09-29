72097773605f7c1be31883a6c68799daf126d160
veloblank <veloblank@gmail.com>
2018-09-29 11:36:05 -0400

    Refactor helpers/session hash use after PROJECT ASSESSMENT

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,6 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions
-    #use Rack::Flash
     set :session_secret, "my-application-secret-secret"
   end
 
@@ -16,33 +15,42 @@ class ApplicationController < Sinatra::Base
 
   get "/make-admin/:id" do
     @user = User.find_by(id: params["id"])
-    if !is_logged_in?(session) || current_user(session) == @user
+    if !is_logged_in? || current_user == @user
         redirect "/users/dashboard"
     elsif
-      current_user(session).is_admin
+      current_user.is_admin
       @user.is_admin = !@user.is_admin
       @user.save
       redirect "/users/dashboard"
     else
       redirect "/users/dashboard"
     end
+
+    # TODO: refactor helpers and session
   end
 
   get '/logout' do
-    session.clear
-    session["selected_props"] = []    #throws an error when Logged out bc session["selected_props"] is iterated on after the redirect
+    current_user.props.clear
+    current_user.save
+    session.clear       #order matters
     redirect '/'
-    # TODO: Fix the nil class error without reassigning the session hash just before the redirect
   end
 
   helpers do
-    def current_user(session_hash)
-      user = User.find_by(id: session_hash[:user_id])
+
+    def clear_pending_picks
+      binding.pry
+      current_user.pending_picks.clear
+      current_user.save
+    end
+
+    def current_user
+      user = User.find_by(id: session[:user_id])
       return user if user
     end
 
-    def is_logged_in?(session_hash)
-      !!current_user(session_hash)
+    def is_logged_in?
+      !!current_user
     end
   end
 end
