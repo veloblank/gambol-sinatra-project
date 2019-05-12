f7d25871c89fdb2d4792194f5d7947c94b817479
John Blank <veloblank@gmail.com>
2019-05-12 14:14:42 -0400

    Refactor User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :betting_slips
-
   has_many :pending_picks
   has_many :props, through: :pending_picks
-
   validates_uniqueness_of :username, :email
   validates_presence_of :username, :email, :password_digest
-  #serialize :pending_picks
-
-
 end
